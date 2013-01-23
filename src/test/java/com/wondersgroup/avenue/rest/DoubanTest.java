a0b914e9ce4ceb492010d3c45f0f740553274431
Jacky.Li <smilejianhua@outlook.com>
2013-01-23 10:23:31 +0800

    Add Rest

---
+++ src/test/java/com/wondersgroup/avenue/rest/DoubanTest.java
@@ -0,0 +1,11 @@
+package com.wondersgroup.avenue.rest;
+
+import org.springframework.web.client.RestTemplate;
+
+public class DoubanTest {
+
+	public void testRest() {
+		RestTemplate rt = new RestTemplate();
+//		String result = rt.getForObject("https://api.douban.com/v2/book/{id}", responseType)
+	}
+}
